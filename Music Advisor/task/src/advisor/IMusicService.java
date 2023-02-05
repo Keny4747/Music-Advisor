cb87d350dc4bbb80e8676ec30c84a34ba9799da8
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-02-05 17:06:17 -0500

    ...

--- Music Advisor/task/src/advisor/IMusicService.java
+++
@@ -1,9 +0,0 @@
-package advisor;
-
-import java.util.List;
-
-public interface IMusicService {
-    public List<Song> getAllSongs();
-    public Song getSongById(int id);
-    public void addSong(Song song);
-}
--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -10,6 +10,8 @@ public class Main{
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+
         while (true) {
 
             String command = input.nextLine();
