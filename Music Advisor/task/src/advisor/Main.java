3aabf0862feb6f6140d4fcd52c33a1f52395fdf5
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-02-05 17:25:46 -0500

    ...

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -9,7 +9,7 @@ public class Main{
         if (args.length > 1 && args[0].equals("-access")) {
             Authorisation.SERVER_PATH = args[1];
         }
-        
+
         Advisor advisor = new Advisor();
         advisor.start();
     }
